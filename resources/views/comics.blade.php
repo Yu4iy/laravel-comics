de9272cf2e6b5d41aa06d678fed698a22a7026f6
Yu4iy <yurimohyliuk@gmail.com>
2022-01-19 18:52:42 +0100

    ADD: comics-section

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -2,7 +2,20 @@
 @extends('layouts.layouts')
 
 @section('comics')
-	<div class="test">
-		<h1>COMICS</h1>
+<section class="comics">
+	<div class="container">
+		<ul class="comics__items-list">
+			@foreach ($comics as $comics)
+				<li  class=" comics__item">
+					<div class="comics__poster">
+						<img src="{{ $comics['thumb'] }}" alt="">
+					</div>
+					<h2 class="comics__title">{{ $comics['title'] }}</h2>
+				</li>
+			@endforeach
+
+		</ul>
+		<a class="comics__btn" href="">Load More</a>
 	</div>
+</section>
 @endsection
