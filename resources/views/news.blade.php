c5fe9b7af551add637a1679f98df659ae26f8a18
Yu4iy <yurimohyliuk@gmail.com>
2022-01-19 19:41:41 +0100

    ADD: header-logic

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -3,5 +3,49 @@
 @section('news')
 	<div>
 		<h1>NEWS</h1>
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis earum ex consequuntur perspiciatis soluta repellendus quod doloremque fugiat adipisci qui nesciunt, exercitationem aut est architecto illum saepe rem neque fuga?
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis earum ex consequuntur perspiciatis soluta repellendus quod doloremque fugiat adipisci qui nesciunt, exercitationem aut est architecto illum saepe rem neque fuga?
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis earum ex consequuntur perspiciatis soluta repellendus quod doloremque fugiat adipisci qui nesciunt, exercitationem aut est architecto illum saepe rem neque fuga?
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis earum ex consequuntur perspiciatis soluta repellendus quod doloremque fugiat adipisci qui nesciunt, exercitationem aut est architecto illum saepe rem neque fuga?
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis earum ex consequuntur perspiciatis soluta repellendus quod doloremque fugiat adipisci qui nesciunt, exercitationem aut est architecto illum saepe rem neque fuga?
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis earum ex consequuntur perspiciatis soluta repellendus quod doloremque fugiat adipisci qui nesciunt, exercitationem aut est architecto illum saepe rem neque fuga?
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis earum ex consequuntur perspiciatis soluta repellendus quod doloremque fugiat adipisci qui nesciunt, exercitationem aut est architecto illum saepe rem neque fuga?
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis earum ex consequuntur perspiciatis soluta repellendus quod doloremque fugiat adipisci qui nesciunt, exercitationem aut est architecto illum saepe rem neque fuga?
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis earum ex consequuntur perspiciatis soluta repellendus quod doloremque fugiat adipisci qui nesciunt, exercitationem aut est architecto illum saepe rem neque fuga?
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis earum ex consequuntur perspiciatis soluta repellendus quod doloremque fugiat adipisci qui nesciunt, exercitationem aut est architecto illum saepe rem neque fuga?
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis earum ex consequuntur perspiciatis soluta repellendus quod doloremque fugiat adipisci qui nesciunt, exercitationem aut est architecto illum saepe rem neque fuga?
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis earum ex consequuntur perspiciatis soluta repellendus quod doloremque fugiat adipisci qui nesciunt, exercitationem aut est architecto illum saepe rem neque fuga?
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis earum ex consequuntur perspiciatis soluta repellendus quod doloremque fugiat adipisci qui nesciunt, exercitationem aut est architecto illum saepe rem neque fuga?
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis earum ex consequuntur perspiciatis soluta repellendus quod doloremque fugiat adipisci qui nesciunt, exercitationem aut est architecto illum saepe rem neque fuga?
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis earum ex consequuntur perspiciatis soluta repellendus quod doloremque fugiat adipisci qui nesciunt, exercitationem aut est architecto illum saepe rem neque fuga?
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis earum ex consequuntur perspiciatis soluta repellendus quod doloremque fugiat adipisci qui nesciunt, exercitationem aut est architecto illum saepe rem neque fuga?
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis earum ex consequuntur perspiciatis soluta repellendus quod doloremque fugiat adipisci qui nesciunt, exercitationem aut est architecto illum saepe rem neque fuga?
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis earum ex consequuntur perspiciatis soluta repellendus quod doloremque fugiat adipisci qui nesciunt, exercitationem aut est architecto illum saepe rem neque fuga?
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis earum ex consequuntur perspiciatis soluta repellendus quod doloremque fugiat adipisci qui nesciunt, exercitationem aut est architecto illum saepe rem neque fuga?
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis earum ex consequuntur perspiciatis soluta repellendus quod doloremque fugiat adipisci qui nesciunt, exercitationem aut est architecto illum saepe rem neque fuga?
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis earum ex consequuntur perspiciatis soluta repellendus quod doloremque fugiat adipisci qui nesciunt, exercitationem aut est architecto illum saepe rem neque fuga?
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis earum ex consequuntur perspiciatis soluta repellendus quod doloremque fugiat adipisci qui nesciunt, exercitationem aut est architecto illum saepe rem neque fuga?
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis earum ex consequuntur perspiciatis soluta repellendus quod doloremque fugiat adipisci qui nesciunt, exercitationem aut est architecto illum saepe rem neque fuga?
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis earum ex consequuntur perspiciatis soluta repellendus quod doloremque fugiat adipisci qui nesciunt, exercitationem aut est architecto illum saepe rem neque fuga?
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis earum ex consequuntur perspiciatis soluta repellendus quod doloremque fugiat adipisci qui nesciunt, exercitationem aut est architecto illum saepe rem neque fuga?
+		
+		
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis earum ex consequuntur perspiciatis soluta repellendus quod doloremque fugiat adipisci qui nesciunt, exercitationem aut est architecto illum saepe rem neque fuga?
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis earum ex consequuntur perspiciatis soluta repellendus quod doloremque fugiat adipisci qui nesciunt, exercitationem aut est architecto illum saepe rem neque fuga?
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis earum ex consequuntur perspiciatis soluta repellendus quod doloremque fugiat adipisci qui nesciunt, exercitationem aut est architecto illum saepe rem neque fuga?
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis earum ex consequuntur perspiciatis soluta repellendus quod doloremque fugiat adipisci qui nesciunt, exercitationem aut est architecto illum saepe rem neque fuga?
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis earum ex consequuntur perspiciatis soluta repellendus quod doloremque fugiat adipisci qui nesciunt, exercitationem aut est architecto illum saepe rem neque fuga?
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis earum ex consequuntur perspiciatis soluta repellendus quod doloremque fugiat adipisci qui nesciunt, exercitationem aut est architecto illum saepe rem neque fuga?
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	</div>
 @endsection
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,11 +1,25 @@
 <header class="header">
 	<div class="header__container container">
 		<img class="header__logo" src=" {{ asset('images/dc-logo.png') }} " alt="">
-		<a class="header__link" href="{{ route('comics') }}"
-		{{-- @if (Request::route()->getName() === 'comics') class="header__link-active" @endif  --}}
-		>
-			COMICS
-		</a>
-		<a class="header__link" href="{{ route('news') }}">NEWS</a>
+
+		<ul>
+			<li>
+				<a   href="{{ route('comics') }}"
+				@if (Request::route()->getName() === 'comics') class= "active" @endif 
+				>
+					COMICS
+				</a>
+			</li>
+
+			<li>
+				<a  href="{{ route('news') }}"
+				@if ( Request::route()->getName() === 'news') class= "active" @endif
+				>
+					NEWS
+				</a>
+			</li>
+		</ul>
+
 	</div>
+	@dump( Request::route()->getName())
 </header>
