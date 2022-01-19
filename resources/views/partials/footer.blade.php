426d98a1102f0266d04b5ebefb06a36cd67ee43d
Yu4iy <yurimohyliuk@gmail.com>
2022-01-19 18:17:04 +0100

    ADD: footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,27 @@
-<footer>
-	FOOTER
+<footer class="footer">
+	<div class="footer__container container">
+		<div class="sign-up">
+			<a href="">Sign-Up Now!</a>
+		</div>
+		
+		<div class="social">
+			<div class="social__text">Folow Us</div>
+			
+			<a class="social__link" href="">
+				<img src="{{ asset('images/footer-facebook.png') }}" alt="">
+			</a>
+			<a class="social__link" href="">
+				<img src="{{ asset('images/footer-periscope.png') }}" alt="">
+			</a>
+			<a class="social__link" href="">
+				<img src="{{ asset('images/footer-pinterest.png') }}" alt="">
+			</a>
+			<a class="social__link" href="">
+				<img src="{{ asset('images/footer-twitter.png') }}" alt="">
+			</a>
+			<a class="social__link" href="">
+				<img src="{{ asset('images/footer-youtube.png') }}" alt="">
+			</a>
+		</div>
+	</div>
 </footer>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,7 +1,11 @@
 <header class="header">
 	<div class="header__container container">
 		<img class="header__logo" src=" {{ asset('images/dc-logo.png') }} " alt="">
-		<a class="header__link header__link-active" href="{{ route('comics') }}">COMICS</a>
+		<a class="header__link" href="{{ route('comics') }}"
+		{{-- @if (Request::route()->getName() === 'comics') class="header__link-active" @endif  --}}
+		>
+			COMICS
+		</a>
 		<a class="header__link" href="{{ route('news') }}">NEWS</a>
 	</div>
 </header>
