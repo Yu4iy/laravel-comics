615c7af30cfbe93d16795fbc922f1eb42c6e02f4
Yu4iy <yurimohyliuk@gmail.com>
2022-01-20 17:47:05 +0100

    ADD: click logic

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -7,12 +7,14 @@
 <section class="comics">
 	<div class="container">
 		<ul class="comics__items-list">
-			@foreach ($comics as $comics)
+			@foreach ($comics as $index => $comics)
 				<li  class=" comics__item">
-					<div class="comics__poster">
-						<img src="{{ $comics['thumb'] }}" alt="">
-					</div>
-					<h2 class="comics__title">{{ $comics['title'] }}</h2>
+					<a href="{{ route('page',['id' => $index]) }}">
+						<div class="comics__poster">
+							<img src="{{ $comics['thumb'] }}" alt="">
+						</div>
+						<h2 class="comics__title">{{ $comics['title'] }}</h2>
+					</a>
 				</li>
 			@endforeach
 
