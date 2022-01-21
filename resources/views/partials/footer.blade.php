ce430d01c8d303c99cfbf633bd91e1656d11db9f
Yu4iy <yurimohyliuk@gmail.com>
2022-01-21 09:29:41 +0100

    ADD: mobile

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -6,22 +6,23 @@
 		
 		<div class="social">
 			<div class="social__text">Folow Us</div>
-			
-			<a class="social__link" href="">
-				<img src="{{ asset('images/footer-facebook.png') }}" alt=""> {{--<<<<<<<<<<<<<<<SI UTILIZA ASSET(per importare imagine) / --}}
-			</a>
-			<a class="social__link" href="">
-				<img src="{{ asset('images/footer-periscope.png') }}" alt="">
-			</a>
-			<a class="social__link" href="">
-				<img src="{{ asset('images/footer-pinterest.png') }}" alt="">
-			</a>
-			<a class="social__link" href="">
-				<img src="{{ asset('images/footer-twitter.png') }}" alt="">
-			</a>
-			<a class="social__link" href="">
-				<img src="{{ asset('images/footer-youtube.png') }}" alt="">
-			</a>
+			<div>
+				<a class="social__link" href="">
+					<img src="{{ asset('images/footer-facebook.png') }}" alt=""> {{--<<<<<<<<<<<<<<<SI UTILIZA ASSET(per importare imagine) / --}}
+				</a>
+				<a class="social__link" href="">
+					<img src="{{ asset('images/footer-periscope.png') }}" alt="">
+				</a>
+				<a class="social__link" href="">
+					<img src="{{ asset('images/footer-pinterest.png') }}" alt="">
+				</a>
+				<a class="social__link" href="">
+					<img src="{{ asset('images/footer-twitter.png') }}" alt="">
+				</a>
+				<a class="social__link" href="">
+					<img src="{{ asset('images/footer-youtube.png') }}" alt="">
+				</a>
+			</div>
 		</div>
 	</div>
 </footer>
